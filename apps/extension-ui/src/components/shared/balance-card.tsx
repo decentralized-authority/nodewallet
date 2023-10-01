f6a9dc934967a9fa412d8988db192ba53236980a
rBurgett <ryan@burgettdev.net>
2023-10-01 03:59:54 -0400

    Hide or disable header links when sending tx from web integration

--- apps/extension-ui/src/components/shared/balance-card.tsx
+++ apps/extension-ui/src/components/shared/balance-card.tsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { CryptoAccount } from '@nodewallet/types';
-import { truncateAddress } from '../../util';
+import { calledFromContentScript, truncateAddress } from '../../util';
 import { ErrorHandlerContext } from '../../hooks/error-handler-context';
 import { RootState } from '../../store';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { RouteBuilder } from '@nodewallet/util-browser';
 
 export interface BalanceCardProps {
@@ -15,10 +15,12 @@ export interface BalanceCardProps {
 }
 export const BalanceCard = ({ walletId, account, hideButtons, backRoute }: BalanceCardProps) => {
 
+  const location = useLocation();
   const errorHandler = useContext(ErrorHandlerContext);
   const {
     accountBalances,
   } = useSelector(({ appState }: RootState) => appState);
+  const fromContentScript = calledFromContentScript(location);
 
   const styles = {
     button: {
@@ -46,7 +48,7 @@ export const BalanceCard = ({ walletId, account, hideButtons, backRoute }: Balan
     <div className={'card mb-0'}>
       <div className={`card-body pt-2 ${hideButtons ? 'pb-0' : 'pb-2'} ps-2 pe-2`}>
         <h5 className={'d-flex flex-row justify-content-between align-items-center mt-0 mb-0'}>
-          <div><Link to={backRoute} title={'View wallets'}><i className={'mdi mdi-menu-left'} />{account.name}</Link></div>
+          <div><Link to={backRoute} title={'View wallets'} onClick={e => fromContentScript ? e.preventDefault() : null}><i className={`mdi mdi-menu-left ${fromContentScript ? 'd-none' : ''}`} />{account.name}</Link></div>
           <div className={'font-monospace'}>{truncateAddress(account.address)} <a href={'#'} title={'Copy address'} onClick={onCopyAddressClick}><i className={'mdi mdi-content-copy'} /></a> <a href={`https://poktscan.com/account/${account.address}`} target={'_blank'} title={'Open in POKTscan'}><i className={'mdi mdi-open-in-new'} /></a></div>
         </h5>
         <div className={'d-flex flex-row justify-content-center pt-3 pb-3'}>
--- apps/extension-ui/src/components/shared/navbar.tsx
+++ apps/extension-ui/src/components/shared/navbar.tsx
@@ -8,6 +8,7 @@ import { ChainType, LocalStorageKey } from '@nodewallet/constants';
 import { setActiveChain } from '../../reducers/app-reducer';
 import { useLocation, useMatches, useNavigate } from 'react-router-dom';
 import { RouteBuilder } from '@nodewallet/util-browser';
+import { calledFromContentScript } from '../../util';
 
 export const Navbar = () => {
 
@@ -19,6 +20,7 @@ export const Navbar = () => {
   const {
     activeChain,
   } = useSelector(({ appState }: RootState) => appState);
+  const fromContentScript = calledFromContentScript(location);
 
   const sendPattern = RouteBuilder.send.generatePathPattern();
   const accountDetailPatt = RouteBuilder.accountDetail.generatePathPattern();
@@ -60,9 +62,15 @@ export const Navbar = () => {
     || accountDetailPatt.test(location.pathname)
     || walletsPatt.test(location.pathname);
 
+  const styles = {
+    nav: {
+      minHeight: 36,
+    },
+  };
+
   return (
-    <nav className="navbar bg-body-tertiary pt-0 pb-0 ps-2 pe-2 d-flex flex-row justify-content-start align-items-center">
-      <a href={'#'} title={'Pocket Network'}>
+    <nav className="navbar bg-body-tertiary pt-0 pb-0 ps-2 pe-2 d-flex flex-row justify-content-start align-items-center" style={styles.nav}>
+      <a href={'#'} title={'Pocket Network'} onClick={e => e.preventDefault()}>
         <img src={'/images/coins/pokt.png'} alt="Pocket" height={30} />
       </a>
       <div className={'flex-grow-1'} />
@@ -70,7 +78,7 @@ export const Navbar = () => {
         <h4 className={'ms-1 mt-0 mb-0 dropdown-center'}>
           <a
             href={'#'}
-            title={'Mainnet'}
+            title={'Select chain'}
             className={'dropdown-toggle'}
             data-bs-toggle="dropdown"
             ref={(node) => {
@@ -79,22 +87,26 @@ export const Navbar = () => {
               }
             }}
           >{activeChain}</a>
-          <ul className={'dropdown-menu'}>
-            {[ChainType.MAINNET, ChainType.TESTNET]
-              .map((chainType) => {
-                return (
-                  <li key={chainType}><a className="dropdown-item" href="#" onClick={e => onChainClick(e, chainType)}>{chainType}</a></li>
-                );
-              })
-            }
-          </ul>
+          {fromContentScript ?
+            <ul className={'dropdown-menu'} />
+            :
+            <ul className={'dropdown-menu'}>
+              {[ChainType.MAINNET, ChainType.TESTNET]
+                .map((chainType) => {
+                  return (
+                    <li key={chainType}><a className="dropdown-item" href="#" onClick={e => onChainClick(e, chainType)}>{chainType}</a></li>
+                  );
+                })
+              }
+            </ul>
+          }
         </h4>
         :
         null
       }
       <div className={'flex-grow-1'} />
       {/*<a href={'#'} title={'Menu'} onClick={onMenuClick}><i className={'mdi mdi-menu fs-2'} /></a>*/}
-      <a href={'#'} title={'Lock Wallets'} onClick={onLockClick} className={'ms-1'}><i className={'mdi mdi-lock-outline fs-2'} /></a>
+      <a href={'#'} title={'Lock Wallets'} onClick={onLockClick} className={'ms-1'}><i className={`mdi mdi-lock-outline fs-2 ${fromContentScript ? 'd-none' : ''}`} /></a>
     </nav>
   );
 };
